39b0ff8803d45d8abcea579a431b64aaaba74f9e
Satoshi Yamamoto <satoshi_yamamoto@cyberagent.co.jp>
2017-04-13 14:40:13 +0900

    Ejected from create-react-app.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import TestUtils from 'react-dom/test-utils';
+import { createRenderer } from 'react-test-renderer/shallow';
 import App from './App';
+import logo from './logo.svg';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+it('should render App component', () => {
+  const renderer = createRenderer();
+  renderer.render(<App/>);
+
+  expect(
+    renderer.getRenderOutput()
+  ).toEqual(
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Welcome to React</h2>
+      </div>
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+    </div>
+  );
 });
